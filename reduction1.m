5d0ace6a22662c8d0d1d48af69daf428a7ff83a1
Ross Parker <rosshparker@gmail.com>
2022-07-26 16:37:55 -0500

    added reduction from Ross

---
+++ reduction1.m
@@ -0,0 +1,74 @@
+addpath( genpath('dataBuilders') );
+
+% generate spike data
+bin_size = 0.01/14;
+assembly_size = 5;
+
+[spikeTimes,Y,t,T,position] = build_spike_data_noise(0.9, assembly_size, 0, 5000, bin_size, 8);
+T = T - T(1);
+
+%% delay coordinates
+
+% find starting times for runs
+t_inds = zeros(size(t));
+for ind = 1:length(t)
+    [~,t_inds(ind)] = min(abs(T - t(ind)));
+end
+
+% first run
+run_length = 20;
+Y1 = Y(:, t_inds(1):t_inds(1)+run_length/bin_size);
+T1 = T(t_inds(1):t_inds(1)+run_length/bin_size);
+
+% average of runs
+Yavg = 0*Y1;
+for ind = 1:length(t)-1
+    Yavg = Yavg + Y(:, t_inds(ind):t_inds(ind)+run_length/bin_size);
+end
+
+% % all runs
+% [delayData, newTimes] = build_delay_coordinates(Y',T,1,2);
+
+% % first run
+% [delayData, newTimes] = build_delay_coordinates(Y1',T1,1,2);
+
+% average of all runs
+[delayData, newTimes] = build_delay_coordinates(Yavg',T1,1,2);
+
+%% SVD
+[U,S,V] = svd(delayData,'econ');
+
+%% plot interesting stuff
+
+% theta rhythm
+thetas = 1:(1/7):200;
+thetay = ones(size(thetas));
+thetay(1:2:end) = 0;
+
+figure;
+
+% plot eigen-timeseries
+ind = 1;            % index of eigen-timeseries
+subplot(2,1,1);
+plot(newTimes, abs( U(:,ind) ) );
+hold on;
+% superimpose theta rhythm
+stairs( thetas, max(abs( U(:,ind) ) )*thetay, 'r' );
+xlim([t(1) t(1)+20]);
+
+% plot spike data
+subplot(2,1,2);
+hold on;
+for ind = 1:assembly_size
+    plot( newTimes, ind+(delayData(:,ind)>0 ), 'k');
+end
+% superimpose theta rhythm
+stairs( thetas, 6*thetay, 'r' );
+xlim([t(1) t(1)+20]);
+
+% plot animal position
+% subplot(3,1,3);
+% plot(T, position );
+% xlim([t(1) t(1)+20]);
+
+
