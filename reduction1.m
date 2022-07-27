f32487240c54171ae3edea20b2dbec4ba6cf23fc
Ross Parker <rosshparker@gmail.com>
2022-07-27 11:44:46 -0500

    added smoothing

--- reduction1.m
+++ reduction1.m
@@ -1,7 +1,7 @@
 addpath( genpath('dataBuilders') );
 
 % generate spike data
-bin_size = 0.01/14;
+bin_size = 0.005/14;
 assembly_size = 5;
 
 [spikeTimes,Y,t,T,position] = build_spike_data_noise(0.9, assembly_size, 0, 5000, bin_size, 8);
@@ -25,6 +25,7 @@ Yavg = 0*Y1;
 for ind = 1:length(t)-1
     Yavg = Yavg + Y(:, t_inds(ind):t_inds(ind)+run_length/bin_size);
 end
+Yavg = Yavg / (length(t)-1);
 
 % % all runs
 % [delayData, newTimes] = build_delay_coordinates(Y',T,1,2);
@@ -33,10 +34,13 @@ end
 % [delayData, newTimes] = build_delay_coordinates(Y1',T1,1,2);
 
 % average of all runs
-[delayData, newTimes] = build_delay_coordinates(Yavg',T1,1,2);
+[delayData, newTimes] = build_delay_coordinates(Yavg',T1,1,1);
+
+smoothDelayData = smoothdata( delayData, 'gaussian', 200);
 
 %% SVD
-[U,S,V] = svd(delayData,'econ');
+% [U,S,V] = svd(delayData,'econ');
+[U,S,V] = svd(smoothDelayData,'econ');
 
 %% plot interesting stuff
 
@@ -48,12 +52,12 @@ thetay(1:2:end) = 0;
 figure;
 
 % plot eigen-timeseries
-ind = 1;            % index of eigen-timeseries
+eig_ind = 1;            % index of eigen-timeseries
 subplot(2,1,1);
-plot(newTimes, abs( U(:,ind) ) );
+plot(newTimes, abs( U(:,eig_ind) ) );
 hold on;
 % superimpose theta rhythm
-stairs( thetas, max(abs( U(:,ind) ) )*thetay, 'r' );
+stairs( thetas, max(abs( U(:,eig_ind) ) )*thetay, 'r' );
 xlim([t(1) t(1)+20]);
 
 % plot spike data
@@ -70,5 +74,13 @@ xlim([t(1) t(1)+20]);
 % subplot(3,1,3);
 % plot(T, position );
 % xlim([t(1) t(1)+20]);
+
+%%
+
+[pks,locs] = findpeaks(abs( U(:,eig_ind) ), 'MinPeakHeight',0.005);
+pktimes = newTimes(locs);
+figure;
+plot(mod(pktimes, 1/7))
+
 
 
